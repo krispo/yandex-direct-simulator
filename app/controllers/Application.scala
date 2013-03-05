42a4ccfc4259bed4c7d2918b01172411ec977540
krispo <konstantin.skipor@gmail.com>
2013-03-05 17:43:13 +0400

    added quartz sheduler, updated web interface with bootstrap

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,38 @@
 package controllers
 
+import jobs._
+
 import play.api._
 import play.api.mvc._
 
+import play.api.data._
+import play.api.data.Forms._
+
+case class Prior(
+  val min: Double,
+  val max: Double,
+  val pMin: Double,
+  val pMax: Double)
+
+case class DBParams(
+  val nCampaigns: Integer,
+  val nfBanners: Integer,
+  val prior: Prior,
+  val nPhrasesInBanner: Integer)
+
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(Scheduler.isStarted & !Scheduler.isInStandbyMode))
+  }
+
+  def startJobs(username: String) = Action {
+    Scheduler.start
+    Ok(views.html.index(Scheduler.isStarted & !Scheduler.isInStandbyMode))
+  }
+
+  def stopJobs(username: String) = Action {
+    Scheduler.stop
+    Ok(views.html.index(Scheduler.isStarted & !Scheduler.isInStandbyMode))
   }
 }
--- app/jobs/Scheduler.scala
+++ app/jobs/Scheduler.scala
@@ -1,29 +1,84 @@
 package jobs
 
-import scala.concurrent.duration._
-import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.concurrent.Akka
 import controllers._
 import models._
-import java.util.Date
-import java.util.concurrent.TimeUnit
-
-import play.api.Play.current //or use (implicit app: play.api.Application)
-
-import org.joda.time.DateTime
+import org.quartz._
+import org.quartz.impl.StdSchedulerFactory
 
 object Scheduler {
 
-  def start {
-    Akka.system.scheduler.schedule(0 seconds, 1 minutes) {
+  val scheduler = StdSchedulerFactory.getDefaultScheduler()
+
+  val jKey = JobKey.jobKey("job")
+  val tKey = TriggerKey.triggerKey("trigger")
+
+  def start = {
+    scheduler.clear() //remove all jobs and triggers
+    scheduler.start()
+
+    println("================== SHEDULER start ==================")
+
+    // define the job and tie it to our executeBlock class
+    val job = JobBuilder.newJob(classOf[executeBlock]).withIdentity(jKey).build()
+
+    // Trigger the job to run now, and then repeat every 10 seconds
+    val trigger = TriggerBuilder.newTrigger()
+      .withIdentity(tKey)
+      .startNow()
+      .withSchedule(
+        SimpleScheduleBuilder.simpleSchedule()
+          .withIntervalInSeconds(1)
+          .repeatForever())
+      .build()
+
+    // Tell quartz to schedule the job using our trigger
+    println(scheduler.scheduleJob(job, trigger))
+
+  }
+
+  def stop = {
+    scheduler.standby()
+    println("================== SHEDULER stop ==================")
+  }
+
+  def shutdown = {
+    scheduler.shutdown()
+    println("================== SHEDULER shutdown ==================")
+  }
+
+  def isStarted: Boolean = scheduler.isStarted()
+  def isInStandbyMode: Boolean = scheduler.isInStandbyMode()
+}
+
+class executeBlock extends Job {
+  def execute(jec: JobExecutionContext) {
+    println("------------------ START Job ------------------")
+    println("------------------ END Job ------------------")
+  }
+
+} 
+
+
+//import scala.concurrent.duration._
+//import play.api.libs.concurrent.Execution.Implicits._
+//import play.api.libs.concurrent.Akka
+//import java.util.Date
+//import java.util.concurrent.TimeUnit
+
+//import play.api.Play.current //or use (implicit app: play.api.Application)
+ /*def start: akka.actor.Cancellable = { //(implicit app: play.api.Application) {
+    val aks = Akka.system
+    val can = aks.scheduler.schedule(5 seconds, 5 seconds) {
       println("!!! START Job ============================================================================ !!!")
       //executeBlock
       println("!!! END Job ============================================================================== !!!")
     }
+    can
   }
 
   def shutdown {
     println("!!! SCHEDULER has STOPPED !!!")
     Akka.system.shutdown()
-  }
-}
+  }*/
+
+//if (!scheduler.checkExists(jKey) & !scheduler.checkExists(tKey))
