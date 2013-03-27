af5500e943540b349b3d78fd6fe3730870cd7782
krispo <konstantin.skipor@gmail.com>
2013-03-27 18:26:19 +0400

    updated charts with campaign ctr

--- app/dao/squerylorm/Charts.scala
+++ app/dao/squerylorm/Charts.scala
@@ -7,23 +7,23 @@ object Charts {
   //Budget evolution in time
   def getBudget(oc: Option[Campaign]): List[(Long, Double)] =
     oc map {
-      c => c.budgetHistory.map(bh => (new DateTime(bh.date).getMillis(), bh.budget)).reverse.tail
+      c => c.budgetHistory.map(bh => (bh.date.getMillis(), bh.budget)).reverse.tail
     } getOrElse (Nil)
 
   //Campaign CTR evolution in time with cumulative clicks and shows
-  def getCampaignCTR(oc: Option[Campaign]): List[(DateTime, Double, Double, Double)] = {
+  def getCampaignCTR(oc: Option[Campaign]): List[(Long, Double, Double, Double)] = {
     oc map { c =>
-      val cp = c.performanceHistory
+      val cp = c.performanceHistory.reverse
       val cClicksContext = cp.map(_.clicks_context).scan(0)(_ + _).tail
       val cClicksSearch = cp.map(_.clicks_search).scan(0)(_ + _).tail
       val cShowsContext = cp.map(_.impress_context).scan(0)(_ + _).tail
       val cShowsSearch = cp.map(_.impress_search).scan(0)(_ + _).tail
 
-      val p = cp.map(_.dateTime).zipWithIndex
+      val p = cp.map(_.date).zipWithIndex
 
       p map {
         case (dt, i) =>
-          (dt, //DateTime
+          (dt.getMillis(), //DateTime
             ctr(cClicksSearch(i), cShowsSearch(i)), //CTR search
             ctr(cClicksContext(i), cShowsContext(i)), //CTR context
             ctr(cClicksSearch(i) + cClicksContext(i), cShowsSearch(i) + cShowsContext(i)) //CTR SUM
